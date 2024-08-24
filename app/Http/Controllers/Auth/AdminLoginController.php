46a98003080c6358f48ecbaa193e74d320d4f102
Michael Ozoudeh <jesmikky@gmail.com>
2024-08-25 00:59:22 +0100

    completed auto company information updates

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -30,6 +30,7 @@ class AdminLoginController extends Controller
     {
        
         $credentials = $request->only('email', 'password');
+        dd(Auth('admin')->attempt($credentials));
         if(Auth('admin')->attempt($credentials)){
             $request->session()->regenerate();
             // $user = Admin::whereId(auth('admin')->user()->id)->first();
