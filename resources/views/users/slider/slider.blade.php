b0c8944d2d77767f614d0533f55988efd2b7032b
Michael Ozoudeh <jesmikky@gmail.com>
2024-08-26 16:27:33 +0100

    files upload fixed

--- resources/views/users/slider/slider.blade.php
+++ resources/views/users/slider/slider.blade.php
@@ -10,8 +10,8 @@
                 <div class="container-no-round">
                     <div class="ps-banner__block">
                         <div class="ps-banner__content">
-                            <h4 class="ps-banner__title text-{{ $slider->color }}">{!! $slider->title !!}</h4>
-                            <div class="ps-banner__desc text-{{ $slider->color }}">{{ $slider->content }}</div>
+                            <h4 class="ps-banner__title text-{{$slider->color}}">{!! $slider->title !!}</h4>
+                            <div class="ps-banner__desc text-{{$slider->color}}">{{ $slider->content }}</div>
                             <div class="ps-banner__btn-group">
                             </div>
                            
