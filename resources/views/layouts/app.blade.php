a4b3c683b7c41a60b14961a4fb7f945f0ad458a9
Michael Ozoudeh <jesmikky@gmail.com>
2024-08-28 16:32:31 +0100

    files upload fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,6 +89,19 @@ toastr.success(message);
     toastr.error(message);
 }        
      </script>
+     <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/66cf30fcea492f34bc0b053e/1i6clenk6';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 </html>
 
